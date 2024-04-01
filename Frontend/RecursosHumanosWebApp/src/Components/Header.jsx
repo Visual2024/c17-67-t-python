d4ba7bc1dde6fabfac9beb2c6defa5935d283deb
githubfacu <elorzfacundo@gmail.com>
2024-04-01 15:05:17 -0300

    ajustes basicos

---
+++ Frontend/RecursosHumanosWebApp/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1 class="text-3xl font-bold underline">Proyecto no Country</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ Frontend/RecursosHumanosWebApp/src/Pages/Perfil.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Perfil = () => {
+  return (
+    <div>
+        <h2 style={{color: 'black'}}>Perfil de usuario</h2>
+    </div>
+  )
+}
+
+export default Perfil
