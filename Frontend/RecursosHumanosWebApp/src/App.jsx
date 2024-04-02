21db0e64ddc4ec1643fff5fffa2270320587ff52
Lucas Gotz <tutigotz@gmail.com>
2024-04-02 19:46:46 -0300

    Tabla de candidatos agregada
    - Tabla de candidatos agregada consumiendo la API del backend +
    react-data-table-component para renderizar los candidatos
    - Fix: Cambio en el settings.py en el back por error de cors al consumir
    la API

--- Frontend/RecursosHumanosWebApp/src/App.jsx
+++ Frontend/RecursosHumanosWebApp/src/App.jsx
@@ -5,24 +5,25 @@ import "./Styles/App.css";
 import Perfil from "./Pages/Perfil";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
+import { Candidates } from "./Pages/Candidates";
 
 
 function App() {
 
 
-    return (
-        <>
-            <Header />
+  return (
+    <>
+      <Header />
+      <MenuLateral />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/perfildeusuario" element={<Perfil />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/candidates" element={<Candidates />} />
+      </Routes>
 
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/perfildeusuario" element={<Perfil />} />
-                <Route path="/login" element={<Login />} />
-            </Routes>
-
-            <MenuLateral />
-        </>
-    );
+    </>
+  );
 }
 
 export default App;
