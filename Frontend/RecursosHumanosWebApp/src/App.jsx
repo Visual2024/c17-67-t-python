4b1ba7ea1b134c93844d44de8f240e2dcf99c481
githubfacu <elorzfacundo@gmail.com>
2024-04-02 02:32:00 -0300

    home y login page

--- Frontend/RecursosHumanosWebApp/src/App.jsx
+++ Frontend/RecursosHumanosWebApp/src/App.jsx
@@ -3,14 +3,21 @@ import Header from "./Components/Header";
 import MenuLateral from "./Components/MenuLateral";
 import "./Styles/App.css";
 import Perfil from "./Pages/Perfil";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+
 
 function App() {
+
+
     return (
         <>
             <Header />
 
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/perfildeusuario" element={<Perfil />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
 
             <MenuLateral />
