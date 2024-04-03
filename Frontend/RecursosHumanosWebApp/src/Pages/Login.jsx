77dc04370fcfd339125f63e0a67e326c3c915f08
Lucas Gotz <tutigotz@gmail.com>
2024-04-03 11:59:06 -0300

    Fix de errores
    - Erroes en las rutas de las importaciones arreglado

--- Frontend/RecursosHumanosWebApp/src/Pages/Login.jsx
+++ Frontend/RecursosHumanosWebApp/src/Pages/Login.jsx
@@ -1,5 +1,5 @@
 
-import FormularioLogin from '../Components/LogIn/FormularioLogin'
+import FormularioLogin from '../Components/Form/FormularioLogin'
 import '../Styles/login.css'
 
 const Login = () => {
@@ -7,26 +7,26 @@ const Login = () => {
 
   return (
     <div className='Container'>
-        <h1 style={{color: 'red'}}>HR Nexo Recursos Humanos</h1>
+      <h1 style={{ color: 'red' }}>HR Nexo Recursos Humanos</h1>
 
-        <div className='Formularios'>
-          <div className='Form-container'>
-            <form action="">
-              <h2>Trabaja con nosotros!</h2>
-              <select name="" id="">
-                <option value="">Selecciona un puesto de trabajo</option>
-                <option value="">Frontend</option>
-                <option value="">Backend</option>
-              </select>
-              <button>Registrate</button>
-            </form>
-            <img src="" alt="" />
-          </div>
-
-          <div className='Form-Login'>
-            <FormularioLogin />
-          </div>
+      <div className='Formularios'>
+        <div className='Form-container'>
+          <form action="">
+            <h2>Trabaja con nosotros!</h2>
+            <select name="" id="">
+              <option value="">Selecciona un puesto de trabajo</option>
+              <option value="">Frontend</option>
+              <option value="">Backend</option>
+            </select>
+            <button>Registrate</button>
+          </form>
+          <img src="" alt="" />
         </div>
+
+        <div className='Form-Login'>
+          <FormularioLogin />
+        </div>
+      </div>
 
     </div>
   )
