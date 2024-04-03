2f4bea777486b4b1f2eb1ef6148dee7a7cec34f4
Piero Ramirez <estibensmax2022@gmail.com>
2024-04-02 19:14:19 -0500

    Formularios de ingresos

--- Frontend/RecursosHumanosWebApp/src/App.jsx
+++ Frontend/RecursosHumanosWebApp/src/App.jsx
@@ -7,13 +7,13 @@ import Login from "./Pages/Login";
 import Home from "./Pages/Home";
 
 
+
 function App() {
 
 
     return (
         <>
             <Header />
-
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/perfildeusuario" element={<Perfil />} />
