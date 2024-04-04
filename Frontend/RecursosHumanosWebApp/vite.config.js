a8f3361e432454c0171e7fcbea4e398809fb0d6a
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2024-04-03 23:39:29 -0300

    Revert "Se actualiza configuración de vite"
    This reverts commit 111b8e8e824f0df0e29db3df524bbe7544ce6470.

--- Frontend/RecursosHumanosWebApp/vite.config.js
+++ Frontend/RecursosHumanosWebApp/vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-});
+})
