f62696815c54cd862416c9f33374e486f0d094a7
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2024-04-14 17:59:07 -0400

    django.db.backends.postgresql_psycopg2

--- Backend/SGRH/settings.py
+++ Backend/SGRH/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         # "ENGINE": "django.db.backends.sqlite3",
         # "NAME": BASE_DIR / "db.sqlite3",
         # Deployment purposes
-        "ENGINE": os.environ.get("DBENGINE"),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.environ.get("PGDATABASE"),
         "HOST": os.environ.get("PGHOST"),
         "USER": os.environ.get("PGUSER"),
