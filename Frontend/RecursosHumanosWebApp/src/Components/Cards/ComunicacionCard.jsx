3b56f0e6e007e00d926e801620cdcf877bf60ed7
Alejandro Luna <135073545+AlejandroLunaDev@users.noreply.github.com>
2024-04-16 10:21:02 -0300

    estructura home vento grid

--- Frontend/RecursosHumanosWebApp/src/Components/Cards/ComunicacionCard.jsx
+++ Frontend/RecursosHumanosWebApp/src/Components/Cards/ComunicacionCard.jsx
@@ -1,7 +1,53 @@
+/* eslint-disable react/prop-types */
+import { Comunicación } from "@/../public/img/Categorias";
+import { useState, useEffect } from "react";
 
+const comunicados = [
+  { id: 1, titulo: "Nuevo horario de trabajo" },
+  { id: 2, titulo: "Recordatorio: Reunión de equipo" },
+  { id: 3, titulo: "Actualización de políticas internas" },
+  { id: 4, titulo: "Felicidades al equipo por los logros recientes" }
+];
 
-export default function ComunicacionCard() {
+export default function ComunicacionCard({ rol }) {
+  const [comunicadosList, setComunicadosList] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setComunicadosList(comunicados);
+    }, 1000);
+  }, []);
+
+  const handleEliminarComunicado = id => {
+    const nuevosComunicados = comunicadosList.filter(comunicado => comunicado.id !== id);
+    setComunicadosList(nuevosComunicados);
+  };
+
   return (
-    <div>ComunicacionCard</div>
-  )
+    <div className="relative shadow-[4px_5px_10px_1px_rgba(0,0,0,0.3)] w-96 h-48 rounded-xl p-4">
+      <header className="flex items-center gap-1 p-2">
+        <Comunicación height="25px" />
+        <h1 className="pb-1">Anuncios</h1>
+      </header>
+      <div className="p-2 max-h-28 overflow-y-auto">
+        <ul>
+          {comunicadosList.length > 0 ? (
+            comunicadosList.map(comunicado => (
+              <li key={comunicado.id} className="text-sm flex items-center gap-1 mb-1">
+                <Comunicación height="15px" />
+                <small className="text-blue-950 font-medium">{comunicado.titulo}</small>
+                {(rol === "GERENTE" || rol === "ADMIN") && (
+                  <button onClick={() => handleEliminarComunicado(comunicado.id)} className="ml-auto text-red-700">
+                    X
+                  </button>
+                )}
+              </li>
+            ))
+          ) : (
+            <li className="absolute top-1/2 left-1/2 -translate-x-1/2">No hay anuncios recientes</li>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 }
--- Frontend/RecursosHumanosWebApp/src/Pages/Home.jsx
+++ Frontend/RecursosHumanosWebApp/src/Pages/Home.jsx
@@ -3,19 +3,16 @@ import ComunicacionCard from "../Components/Cards/ComunicacionCard";
 import { NameCard } from "../Components/Cards/NameCard";
 import ProyectosActivosCard from "../Components/Cards/ProyectosActivosCard";
 import QuienEstaAdentroCard from "../Components/Cards/QuienEstaAdentroCard";
+import Incidentes from "../Components/Cards/Incidentes";
+import SolicitudVacaciones from "../Components/Cards/SolicitudVacaciones";
 
 export function Home() {
-  const [usuario, setUsuario] = useState(null);
   const [rol, setRol] = useState(null);
 
   useEffect(() => {
-    const usuarioSessionStorage = JSON.parse(
-      sessionStorage.getItem("nombreUsuario")
-    );
     const rolSessionStorage = JSON.parse(sessionStorage.getItem("rol"));
 
-    if (usuarioSessionStorage && rolSessionStorage) {
-      setUsuario(usuarioSessionStorage);
+    if (rolSessionStorage) {
       setRol(rolSessionStorage);
     } else {
       window.location.replace("/login");
@@ -23,16 +20,40 @@ export function Home() {
   }, []);
 
   return (
-    <section>
-      {rol === "GERENTE"|| rol === "ADMIN" ? (
+    <section className="flex justify-center">
+      {rol === "GERENTE" || rol === "ADMIN" ? (
+        <section
+          style={{
+            display: "grid",
+            gridTemplateColumns: "288px 384px 384px",
+            gridTemplateRows: "136px 1fr 1fr",
+            gap:'1rem'
+          }}
+        >
+          <div style={{ gridColumn: "1 / 2", gridRow: "1 / 2" }}>
+            <NameCard />
+          </div>
+          <div style={{ gridColumn: "2 / 3", gridRow: "1 / 2" }}>
+            <ComunicacionCard rol={rol} />
+          </div>
+          <div style={{ gridColumn: "3 / 4", gridRow: "1 / 4" }}>
+            <QuienEstaAdentroCard />
+          </div>
+          <div className=" mt-12" style={{ gridColumn: "2 / 2", gridRow: "2 / 4" }}>
+            <ProyectosActivosCard />
+          </div>
+          <div className="" style={{ gridColumn: "1 / 2", gridRow: "2 / 4" }}>
+            <Incidentes />
+          </div>
+          <div className="" style={{ gridColumn: "1 / 2", gridRow: "3 / 4" }}>
+            <SolicitudVacaciones />
+          </div>
+        </section>
+      ) : (
         <>
           <NameCard />
-          <QuienEstaAdentroCard />
-          <ComunicacionCard />
-          <ProyectosActivosCard />
+          <ComunicacionCard rol={rol} />
         </>
-      ) : (
-        <NameCard />
       )}
     </section>
   );
