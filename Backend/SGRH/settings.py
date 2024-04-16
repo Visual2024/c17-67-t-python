adb7184454410aed6adb6584aba8650529b07638
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2024-04-16 18:25:28 -0400

    Allowing all origins on CORS config

--- Backend/SGRH/settings.py
+++ Backend/SGRH/settings.py
@@ -155,14 +155,15 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "frontend/build/static"),)
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # CORS Origins Authorization
-CORS_ALLOW_ALL_ORIGINS: False  # Set to True to allow all origins
+CORS_ALLOW_ALL_ORIGINS: True  # Set to True to allow all origins
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "https://localhost:9000",
-    "https://127.0.0.1:9000",
-    "https://c17-67-t-python-production.up.railway.app",
+    "*"
+    # "http://localhost:8000",
+    # "http://127.0.0.1:8000",
+    # "https://localhost:9000",
+    # "https://127.0.0.1:9000",
+    # "https://c17-67-t-python-production.up.railway.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = ["https://c17-67-t-python-production.up.railway.app"]
