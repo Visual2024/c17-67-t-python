323637d7a1d57e59543cf83bbdd4f1f72b7d708e
Alejandro Luna <135073545+AlejandroLunaDev@users.noreply.github.com>
2024-04-16 10:24:09 -0300

    eliminacion de api unplash(no sirvio)

--- Frontend/RecursosHumanosWebApp/src/Components/Cards/HeaderCard.jsx
+++ Frontend/RecursosHumanosWebApp/src/Components/Cards/HeaderCard.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from 'react';
+
 import { useContadorTiempo } from '../../Hook/useContadorTiempo';
-import fetchRandomFaceImage from '../../services/api/unsplash/unplash'
+
 
 export function HeaderCard({ nombreUsuario }) {
-  const [imageUrl, setImageUrl] = useState(null);
+/*   const [imageUrl, setImageUrl] = useState(null); */
   const { tiempoFormateado, iniciarContador, pausarContador, reanudarContador, detenerContador, inicio, pausa } = useContadorTiempo();
 
   const obtenerNombreUsuario = (correo) => {
@@ -17,13 +17,7 @@ export function HeaderCard({ nombreUsuario }) {
     }
     return '';
   };
-  useEffect(() => {
-    const getImage = async () => {
-      const url = await fetchRandomFaceImage();
-      setImageUrl(url);
-    };
-    getImage();
-  }, []);
+
 
   return (
     <article className="object-contain flex gap-7 shadow-md p-2 rounded-lg text-xl text-gray-500 font-medium">
@@ -50,10 +44,7 @@ export function HeaderCard({ nombreUsuario }) {
           </button>
         )}
       </div>
-      <div className='flex items-center gap-1'>
-        <img className=' h-7 rounded-full' src={imageUrl} alt="Face" />
-      <h2>{obtenerNombreUsuario(nombreUsuario)}</h2>
-      </div>
+   <h2>{obtenerNombreUsuario(nombreUsuario)}</h2>
     </article>
   );
 }
