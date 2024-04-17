3bc66b0e8e1d5bacb0c62c1afad3f59dead374d4
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2024-04-16 21:02:34 -0400

    corrigiendo cors

--- Backend/SGRH/settings.py
+++ Backend/SGRH/settings.py
@@ -158,7 +158,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOW_ALL_ORIGINS: True  # Set to True to allow all origins
 
 CORS_ALLOWED_ORIGINS = [
-    "*"
     # "http://localhost:8000",
     # "http://127.0.0.1:8000",
     # "https://localhost:9000",
