36d2670585022509059cff33902b9689753e53e3
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2024-04-17 23:22:13 -0400

    adding url for test

--- Backend/SGRH/settings.py
+++ Backend/SGRH/settings.py
@@ -160,15 +160,23 @@ CORS_ALLOW_ALL_ORIGINS: True  # Set to True to allow all origins
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://192.168.100.172:3000",
+    "https://localhost:8000",
+    "https://127.0.0.1:8000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "https://localhost:9000",
     "https://127.0.0.1:9000",
+    "http://localhost:9000",
+    "http://127.0.0.1:9000",
     "https://c17-67-t-python-production.up.railway.app",
 ]
 
-CSRF_TRUSTED_ORIGINS = ["https://c17-67-t-python-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://127.0.0.1:9000",
+    "http://localhost:9000",
+]
+
+CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
