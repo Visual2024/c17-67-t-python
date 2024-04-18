683394cd94bac962ac4bc9bd6d770a4765d9fb59
Lucas Gotz <tutigotz@gmail.com>
2024-04-18 17:09:10 -0300

    Funciones duplicadas borradas + comentarios borrados

--- Frontend/RecursosHumanosWebApp/src/Components/Form/FormularioRegistro.jsx
+++ Frontend/RecursosHumanosWebApp/src/Components/Form/FormularioRegistro.jsx
@@ -1,18 +1,9 @@
-import { useNavigate } from "react-router-dom";
-import {
-    validateName,
-    validateDNI,
-    validateEmail,
-    validatePhoneNumber,
-} from "../../utils/regexValidation";
-import { useState, useContext, useEffect } from "react";
+import { useContext, } from "react";
 import { twMerge } from "tailwind-merge";
 import { FormContext } from "../../Context/FormContext";
-import Swal from "sweetalert2";
+import { getLastName, getPuestoDeTrabajo } from "../../utils/apellidoUtils";
 
 export const FormularioRegistro = () => {
-    // const [candidate, setCandidate] = useState({});
-    // const [error, setError] = useState({ firstState: true });
     const {
         candidate,
         setCandidate,
@@ -24,74 +15,6 @@ export const FormularioRegistro = () => {
         error,
         setError,
     } = useContext(FormContext);
-
-    // const validateFields = (candidate) => {
-    //     setError({
-    //         first_name: !validateName(candidate.first_name),
-    //         last_name: !validateName(candidate.last_name),
-    //         email: !validateEmail(candidate.email),
-    //         dni: !validateDNI(candidate.dni),
-    //         phone_number: !validatePhoneNumber(candidate.phone_number),
-    //     });
-    //     return {
-    //         first_name: !validateName(candidate.first_name),
-    //         last_name: !validateName(candidate.last_name),
-    //         email: !validateEmail(candidate.email),
-    //         dni: !validateDNI(candidate.dni),
-    //         phone_number: !validatePhoneNumber(candidate.phone_number),
-    //     };
-    // };
-
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     if (
-    //         !candidate.first_name ||
-    //         !candidate.last_name ||
-    //         !candidate.email ||
-    //         !candidate.dni
-    //     ) {
-    //         return setError({ allFields: true });
-    //     }
-
-    //     fetch(`http://127.0.0.1:8000/hiring/api/v1/candidate/`, {
-    //         method: "POST",
-    //         body: JSON.stringify(candidate),
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },
-    //     })
-    //         .then((res) => res.json())
-    //         .then((data) => {
-    //             console.log(data);
-    //             Swal.fire({
-    //                 title: "¡Postulación exitosa!",
-    //                 text: "Gracias por postularte",
-    //                 icon: "success",
-    //                 confirmButtonText: "Ok",
-    //             });
-    //             return formSwitch();
-    //         });
-    // };
-
-    const getLastName = (candidate) => {
-        if (candidate.last_name) {
-            let str = candidate.last_name;
-            let words = str.split(" ");
-            words.pop();
-            return words.join(" ");
-        } else {
-            return "";
-        }
-    };
-
-    const getPuestoDeTrabajo = (candidate) => {
-        let str = candidate.last_name;
-        let words = str.split(" ");
-        let puesto = words.find(
-            (word) => word === "Frontend" || word === "Backend"
-        );
-        return puesto;
-    };
 
     return (
         <form className="p-5 flex flex-col gap-2">
@@ -160,34 +83,6 @@ export const FormularioRegistro = () => {
                         </div>
                     </div>
                     <div className="flex gap-2">
-                        {/* <div className="flex flex-col max-w-fit">
-                            <label htmlFor="dni">DNI:</label>
-                            <input
-                                type="text"
-                                className={twMerge(
-                                    "px-2 py-2 border-2 border-gray-300 rounded-md",
-                                    error.dni && "border-red-500"
-                                )}
-                                value={candidate.dni || ""}
-                                onChange={(e) => {
-                                    setError((prevErrors) => ({
-                                        ...prevErrors,
-                                        dni: false,
-                                        allFields: false,
-                                    }));
-                                    return setCandidate({
-                                        ...candidate,
-                                        dni: e.target.value,
-                                    });
-                                }}
-                                placeholder="12345678"
-                            />
-                            {error.dni && (
-                                <p className="text-red-500">
-                                    Algo ha salido mal
-                                </p>
-                            )}
-                        </div> */}
                         <div className="flex flex-col max-w-fit">
                             <label htmlFor="telefono">
                                 Fecha de nacimiento:
@@ -410,6 +305,7 @@ export const FormularioRegistro = () => {
                     )}
                 </div>
             )}
+            {/* Tercer paso */}
             {paso === 3 && (
                 <div className="flex flex-col items-center gap-1">
                     <h4 className="text-2xl font-bold">
@@ -461,12 +357,6 @@ export const FormularioRegistro = () => {
                 )}
             </div>
 
-            {/* <button
-                type="submit"
-                className="py-2 px-5 max-w-fit bg-primary hover:bg-primary/90 text-white rounded-lg"
-            >
-                Enviar
-            </button> */}
         </form>
     );
 };
