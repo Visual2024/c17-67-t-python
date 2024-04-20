df8383e5cad5748bdbd50c38982abcc01f88bc45
Félix Antonio Pacheco Lobos <75093385+Thoraker@users.noreply.github.com>
2024-04-20 11:45:41 -0400

    Actualizar settings.py

--- Backend/SGRH/settings.py
+++ Backend/SGRH/settings.py
@@ -175,7 +175,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://localhost:9000",
     "https://127.0.0.1:9000",
     "http://localhost:9000",
-    "http://127.0.0.1:9000", "https://gestion-de-recursos-humanos-en-la-nube-facundodevs-projects.vercel.app/",
+    "http://127.0.0.1:9000",
+    "https://gestion-de-recursos-humanos-en-la-nube-facundodevs-projects.vercel.app/",
     "https://c17-67-t-python-production.up.railway.app",
 ]
 
