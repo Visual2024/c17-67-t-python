57dca5cd344ec077965e662ce9892f6b6b82c473
Piero Ramirez <estibensmax2022@gmail.com>
2024-04-22 16:28:07 -0500

    Resolviendo conflicto

--- Frontend/RecursosHumanosWebApp/src/Pages/GerenteGestionFinanzas.jsx
+++ Frontend/RecursosHumanosWebApp/src/Pages/GerenteGestionFinanzas.jsx
@@ -2,17 +2,12 @@ import { LinesChart } from "../Components/Gaficos/Lineas";
 import { Pies } from "../Components/Gaficos/PiesChart";
 import { Date_Finanzas } from "../Components/Finanzas/Date_Finanzas";
 import { Date_Finanzas_2 } from "../Components/Finanzas/Date_Finanzas2";
-<<<<<<< HEAD
 import { Dashboard_Gerente1 } from "../Components/Finanzas/Dashboard_Gerente1";
 import { Dashboard_Gerente2 } from "../Components/Finanzas/Dashboard_Gerente2";
-=======
-import { Personal_Info } from "../Components/Finazas_DashBoard/Personal_info";
->>>>>>> frontend-dev
 
 export const GerenteGestionFinanzas = () => {
   return (
-    
-<<<<<<< HEAD
+    <main>
       <section className="flex flex-row mr-10 border-solid  w-[96%] h-[20rem]">
         <div className=" p-3 w-[100%]">
           <LinesChart />
@@ -23,9 +18,5 @@ export const GerenteGestionFinanzas = () => {
           <Dashboard_Gerente1 />
           <Dashboard_Gerente2 />
     </main>
-=======
-      
-    <h1>Gestion del Gerente</h1>
->>>>>>> frontend-dev
   );
 };
