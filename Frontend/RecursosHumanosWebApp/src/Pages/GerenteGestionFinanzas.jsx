58af5cd0b4e79afb67069ff9e88445646703eba1
Piero Ramirez <estibensmax2022@gmail.com>
2024-04-22 16:12:34 -0500

    Agregando tablas

--- Frontend/RecursosHumanosWebApp/src/Pages/GerenteGestionFinanzas.jsx
+++ Frontend/RecursosHumanosWebApp/src/Pages/GerenteGestionFinanzas.jsx
@@ -2,6 +2,8 @@ import { LinesChart } from "../Components/Gaficos/Lineas";
 import { Pies } from "../Components/Gaficos/PiesChart";
 import { Date_Finanzas } from "../Components/Finanzas/Date_Finanzas";
 import { Date_Finanzas_2 } from "../Components/Finanzas/Date_Finanzas2";
+import { Dashboard_Gerente1 } from "../Components/Finanzas/Dashboard_Gerente1";
+import { Dashboard_Gerente2 } from "../Components/Finanzas/Dashboard_Gerente2";
 
 export const GerenteGestionFinanzas = () => {
 
@@ -51,6 +53,8 @@ export const GerenteGestionFinanzas = () => {
       </section>
           <Date_Finanzas/>
           <Date_Finanzas_2/>
+          <Dashboard_Gerente1 />
+          <Dashboard_Gerente2 />
     </main>
   );
 };
