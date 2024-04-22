a044b2a66986e1a3982a983d6d77ffbc0628e201
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2024-04-21 22:27:14 -0400

    adding path

--- Backend/SGRH/urls.py
+++ Backend/SGRH/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include, url
+from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -32,7 +32,7 @@ admin.autodiscover()
 
 urlpatterns = [
     # path("admin/", admin.site.urls),
-    url(r"^admin/", include(admin.site.urls)),
+    path(r"^admin/", include(admin.site.urls)),
     # API Documentation
     path("docs/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
